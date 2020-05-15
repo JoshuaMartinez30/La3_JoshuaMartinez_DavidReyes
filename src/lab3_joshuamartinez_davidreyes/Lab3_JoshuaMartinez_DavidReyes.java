9375c387f219f5f39f6a484fe4ec599192c062d9
Joshua Martinez <Joshuamartinez8723@gmail.com>
2020-05-15 13:38:01 -0600

    Creacion proyecto

---
+++ src/lab3_joshuamartinez_davidreyes/Lab3_JoshuaMartinez_DavidReyes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_joshuamartinez_davidreyes;
+
+/**
+ *
+ * @author joshu
+ */
+public class Lab3_JoshuaMartinez_DavidReyes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
