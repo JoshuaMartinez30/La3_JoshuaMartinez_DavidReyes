61703f876d120f78edb0038195c7bc09cf6d7045
Davidreyesd13 <davidreyesd13@gmail.com>
2020-05-15 16:30:27 -0600

    Clases de Quiosco y bares

--- src/lab3_joshuamartinez_davidreyes/Bares.java
+++ src/lab3_joshuamartinez_davidreyes/Bares.java
@@ -1,5 +1,11 @@
 package lab3_joshuamartinez_davidreyes;
 
+import java.util.ArrayList;
+
 public class Bares extends Locales{
+
+    public Bares(String nombrelocal, ArrayList<Empleados> lista, ArrayList<Productos> listp, Empleados gerente) {
+        super(nombrelocal, lista, listp, gerente);
+    }
     
 }
--- src/lab3_joshuamartinez_davidreyes/Quiosco.java
+++ src/lab3_joshuamartinez_davidreyes/Quiosco.java
@@ -1,5 +1,13 @@
 package lab3_joshuamartinez_davidreyes;
 
+import java.util.ArrayList;
+
 public class Quiosco extends Locales{
+
+    public Quiosco(String nombrelocal, ArrayList<Empleados> lista, ArrayList<Productos> listp, Empleados gerente) {
+        super(nombrelocal, lista, listp, gerente);
+    }
+    
+    
     
 }
