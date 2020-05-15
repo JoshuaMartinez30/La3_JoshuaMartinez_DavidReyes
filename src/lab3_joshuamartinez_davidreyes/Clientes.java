87fcb452b5fb3d08d9aefd860a45c5ddd7f7d50c
Davidreyesd13 <davidreyesd13@gmail.com>
2020-05-15 14:33:24 -0600

    correccion

--- src/lab3_joshuamartinez_davidreyes/Clientes.java
+++ src/lab3_joshuamartinez_davidreyes/Clientes.java
@@ -10,10 +10,10 @@ ArrayList<Productos> compras;
     public Clientes() {
     }
 
-    public Clientes(double dinero, ArrayList<Productos> compras, ArrayList<String> ID, ArrayList<String> username, ArrayList<String> contra, String Correo, Date nacimiento) {
+    public Clientes(double dinero,ArrayList<String> ID, ArrayList<String> username, ArrayList<String> contra, String Correo, Date nacimiento) {
         super(ID, username, contra, Correo, nacimiento);
         this.dinero = dinero;
-        this.compras = compras;
+        
     }
 
     public double getDinero() {
