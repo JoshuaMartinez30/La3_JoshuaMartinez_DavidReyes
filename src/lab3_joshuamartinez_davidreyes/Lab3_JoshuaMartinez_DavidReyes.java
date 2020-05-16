416f4611620ffbf88d63a4191128f54fb5038944
Joshua Martinez <Joshuamartinez8723@gmail.com>
2020-05-15 19:57:19 -0600

    venta

--- src/lab3_joshuamartinez_davidreyes/Lab3_JoshuaMartinez_DavidReyes.java
+++ src/lab3_joshuamartinez_davidreyes/Lab3_JoshuaMartinez_DavidReyes.java
@@ -7,9 +7,9 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Lab3_JoshuaMartinez_DavidReyes {
-
+static int cont=0;
     public static void main(String[] args) throws ParseException {
-        char resp = 's';
+        char resp = 's', resp1='s';
         Scanner input = new Scanner(System.in);
         int opcion, opc2, opc3, opc4, opc5;
         String horario, ID, username, contraseña, correo, f;
@@ -22,15 +22,14 @@ public class Lab3_JoshuaMartinez_DavidReyes {
         ArrayList<Productos> lista3 = new ArrayList();
         ArrayList empleados = new ArrayList();
         ArrayList productos = new ArrayList();
-
-        while (resp == 's' || resp == 'S') {
-            System.out.print("1 log in y sig in");
-            System.out.print("2. Salir");
+        while (resp1 == 's' || resp1 == 'S') {
+            System.out.println("1 log in y sig in");
+            System.out.println("2. Salir");
             System.out.print("Ingrese opcion: ");
             opcion = input.nextInt();
             switch (opcion) {
                 case 1:
-                    System.out.print("1. log in");
+                    System.out.println("1. log in");
                     System.out.println("2. sig in");
                     System.out.print("Ingrese opcion: ");
                     opc2 = input.nextInt();
@@ -39,7 +38,7 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                             int tipo = 0;
                             System.out.print("Ingrese username: ");
                             username = input.next();
-                            System.out.println("Ingrese contraseña: ");
+                            System.out.print("Ingrese contraseña: ");
                             contraseña = input.next();
                             if (username.equalsIgnoreCase("SUDO") && contraseña.equalsIgnoreCase("1234")) {
                                 tipo = 1;
@@ -47,26 +46,26 @@ public class Lab3_JoshuaMartinez_DavidReyes {
 
                             for (Object t : lista) {
                                 if (t instanceof Empleados) {
-                                    String a = String.valueOf(t);
-                                    String con = String.valueOf(t);
-                                    if (username.equalsIgnoreCase(a) && contraseña.equalsIgnoreCase(con)) {
-                                        tipo = 2;
+                                    String a= lista.get(lista.indexOf(t)).getUsername();
+                                    String b = lista.get(lista.indexOf(t)).getContra();
+                                    if (username.equalsIgnoreCase(a) && contraseña.equalsIgnoreCase(b)) {
+                                        tipo=2;
                                     }
                                 }
                             }
                             for (Object o : lista) {
                                 if (o instanceof Clientes) {
-                                    String a = String.valueOf(o);
-                                    String con = String.valueOf(o);
-                                    if (username.equalsIgnoreCase(a) && contraseña.equalsIgnoreCase(con)) {
-                                        tipo = 2;
+                                    String con = lista.get(lista.indexOf(o)).getContra();
+                                    String a = lista.get(lista.indexOf(o)).getUsername();
+                                    if (username.equalsIgnoreCase(a) || contraseña.equalsIgnoreCase(con)) {
+                                        tipo = 3;
                                     }
                                 }
                             }
                             switch (tipo) {
                                 case 1:
                                     while (resp == 's' || resp == 'S') {
-                                        System.out.print("1. crear local");
+                                        System.out.println("1. crear local");
                                         System.out.println("2. crear productos");
                                         System.out.println("3. crear crear empleados");
                                         System.out.println("4. Eliminar");
@@ -74,7 +73,7 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                         opc4 = input.nextInt();
                                         switch (opc4) {
                                             case 1:
-                                                System.out.print("1. crear tienda");
+                                                System.out.println("1. crear tienda");
                                                 System.out.println("2. crear quiosco");
                                                 System.out.println("3. crear bar");
                                                 System.out.print("Ingrese opcion: ");
@@ -82,8 +81,7 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                                 switch (opc3) {
                                                     case 1:
                                                         System.out.print("Ingrese nombre del local: ");
-                                                        input = new Scanner(System.in);
-                                                        nombre = input.nextLine();
+                                                        nombre = input.next();
                                                         for (Object o : lista) {
                                                             if (o instanceof Empleados) {
                                                                 empleados.add(o);
@@ -96,7 +94,7 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                                                 productos.add(u);
                                                             }
                                                         }
-                                                        Tiendas t = new Tiendas(nombre, empleados, productos, empleados.get(0).toString());
+                                                        Tiendas t = new Tiendas(nombre, empleados, productos, nombre);
                                                         lista2.add(t);
                                                         break;
                                                     case 2:
@@ -115,7 +113,7 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                                                 productos.add(u);
                                                             }
                                                         }
-                                                        Quiosco q = new Quiosco(nombre, empleados, productos, empleados.get(0).toString());
+                                                        Quiosco q = new Quiosco(nombre, empleados, productos, nombre);
                                                         lista2.add(q);
                                                         break;
 
@@ -133,7 +131,7 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                                                 productos.add(u);
                                                             } 
                                                         }
-                                                        Bares b = new Bares(nombre, empleados, productos,empleados.get(0).toString() );
+                                                        Bares b = new Bares(nombre, empleados, productos, nombre);
                                                         lista2.add(b);
                                                         break;
                                                 }//fin crear locales
@@ -147,7 +145,24 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                                 opc5 = input.nextInt();
                                                 switch (opc5) {
                                                     
-
+                                                    case 1:
+                                                        String genero, talla;
+                                                        System.out.print("Ingrese genero: ");
+                                                        genero = input.next();
+                                                        System.out.print("Ingrese Talla: ");
+                                                        talla = input.next();
+                                                        System.out.print("Ingrese la descripcion: ");
+                                                        input = new Scanner(System.in);
+                                                        descripcion = input.nextLine();
+                                                        System.out.print("Ingrese el nombre: ");
+                                                        input = new Scanner(System.in);
+                                                        nombre = input.nextLine();
+                                                        System.out.print("Ingrese el Precio: ");
+                                                        precio = input.nextDouble();
+                                                        Ropa r = new Ropa(genero, talla, descripcion, nombre, precio);
+                                                        lista3.add(r);
+                                                        break;
+                                                    
                                                     case 2:
                                                         System.out.print("Ingrese la descripcion: ");
                                                         input = new Scanner(System.in);
@@ -239,20 +254,75 @@ public class Lab3_JoshuaMartinez_DavidReyes {
                                                 
                                                 break;
                                         }
-                                    }//fin menu de gererente
+                                        System.out.print("Desea crear otro[S/N]:");
+                                        resp = input.next().charAt(0);
+                                        
+                                    }//fin menu admin
                                     break;
                                 case 2:
+                                    System.out.println("Bienvenido empleado");
                                     
                                     break;
 
                                 case 3:
-
+                                    System.out.println("Bienvenido cliente");
+                                    System.out.println("1. Tiendas");
+                                    System.out.println("2. Quioscos");
+                                    System.out.println("3. Bares");
+                                    System.out.print("Ingrese el local: ");
+                                    int pos2 = input.nextInt();
+                                    switch (pos2) {
+                                        case 1:
+                                            double dinero, preci;
+                                            for (Object u : lista2) {
+                                                if (u instanceof Tiendas) {
+                                                    System.out.println(lista2.indexOf(u)+") "+u);
+                                                }
+                                            }
+                                            System.out.print("Ingrese la posicion de la tienda: ");
+                                            int pos3 = input.nextInt();
+                                            for (Object t : lista3) {
+                                                if (t instanceof Comida) {
+                                                    
+                                                }else{
+                                                     System.out.println(lista3.indexOf(t)+") "+t);
+                                                }
+                                            }
+                                            System.out.println("Ingrese la posicion del producto que desea comprar: ");
+                                            int pos4=input.nextInt();
+                                            preci = lista3.get(pos4).getPrecio();
+                                            System.out.println(preci);
+                                            dinero = ((Clientes)lista.get(pos4)).getDinero();
+                                            dinero=dinero-preci;
+                                            ((Clientes)lista.get(pos4)).setDinero(dinero);
+                                            
+                                            break;
+                                        
+                                        case 2:
+                                            for (Object u : lista2) {
+                                                if (u instanceof Quiosco) {
+                                                    System.out.println(lista2.indexOf(u)+") "+u);
+                                                }
+                                            }
+                                            break;
+                                            
+                                        case 3:
+                                            for (Object u : lista2) {
+                                                if (u instanceof Bares) {
+                                                    System.out.println(lista2.indexOf(u)+") "+u);
+                                                }
+                                            }
+                                            break;
+                                    }
+                                    
+                                    
                                     break;
 
                                 case 0:
                                     System.out.print("Usuario o  contraseña es incorrecta");
                                     break;
                             }//fin log in
+                    
                             break;
 
                         case 2:
